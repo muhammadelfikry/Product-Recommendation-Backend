78544c1214ea18e9e1088a6b001421e16ea01abe
Muhammad Elfikry <melfikry@gmail.com>
2025-02-05 00:16:22 +0700

    feat(API): add API using FastAPI Serve

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from etl.setup import *
+from etl.extract import extract
+from etl.transform import transform
+from etl.load import load
+import sqlalchemy as sqla
+
+app = FastAPI()
+
+db = sqla.create_engine(src_url)
+
+sql = 'SELECT * FROM product'
+df = extract(db=db, sql=sql)
+
+cosine_sim_df, items = transform(df)
+
+@app.get("/recommendation")
+def get_recommendation(product_name: str):
+    user_input = product_name
+
+    recommendation = load(user_input, similarity_df=cosine_sim_df, items=items)
+    
+    return recommendation
+
+if __name__=="__main__":
+    app.run(debug=True, port=8000)
